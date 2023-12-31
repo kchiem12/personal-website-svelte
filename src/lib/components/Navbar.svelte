e3f2f739616b2784ba0941557301bc7c40d36cf7
Kenneth Chiem <kc696@cornell.edu>
2023-12-31 01:07:48 -0500

    add navbar and basic information

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,38 @@
+<div class="navbar-container">
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/projects">Projects</a></li>
+  </ul>
+</div>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Inter&family=Playfair+Display&display=swap");
+
+  .navbar-container {
+    font-family: "Inter", sans-serif;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    padding-right: 2rem;
+    padding-top: 2rem;
+  }
+
+  ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    gap: 2rem;
+  }
+
+  li {
+    font-size: 1.25rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--color-text);
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<div class="container">
+  <h1 class="name">Kenneth Chiem</h1>
+
+  <p class="introduction">Welcome! A few things about me:</p>
+  <ul>
+    <li>CS student at Cornell University</li>
+    <li>
+      Interests lie in machine learning, full-stack development, and mobile
+      development
+    </li>
+    <li>Philly native</li>
+    <li>
+      <a href="">email</a> and
+      <a href="https://www.linkedin.com/in/kenneth-chiem/">linkedin</a>
+    </li>
+  </ul>
+</div>
+
+<style>
+  .container {
+    font-family: "Inter", sans-serif;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-top: 2rem;
+  }
+
+  .name {
+    font-size: 5rem;
+    margin-bottom: 0;
+  }
+
+  .introduction {
+    font-size: 1.5rem;
+    margin-top: 0;
+  }
+</style>
